16cd308c44857e6edc58d4942c2c1b4ccf26d162
Devansh Sharma <devaton4@gmail.com>
2025-11-26 19:01:43 +0000

    added symbol in 02

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -10,3 +10,11 @@ let weirdType = null
 
 console.log(typeof undef);      // undefined is a 'undefined' type dataset
 console.log(typeof(weirdType)) // null is a 'object' type dataset
+
+
+const id1 = Symbol("12")
+const id2 = Symbol("12")
+
+console.log(typeof(id1));
+
+console.log(id1 === id2);   // false even tho both symbol has same value input, creates a unique value for each variable
