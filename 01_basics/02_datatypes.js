19de9e32ef6f1a84f6888deb8033c3dcab56145d
Devansh Sharma <devaton4@gmail.com>
2025-11-26 19:19:36 +0000

    updated non primitave datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,15 +1,17 @@
+//NON PRIMITIVE DATATYPES-----------------------------------
+
 let stringy = "hello"
 let num = 12
 let bool = false
 
 let undef = undefined
-let weirdType = null
+let nullType = null
 
 
 //                  'typeof(bool)' and 'typeof bool' return same result
 
 console.log(typeof undef);      // undefined is a 'undefined' type dataset
-console.log(typeof(weirdType)) // null is a 'object' type dataset
+console.log(typeof(nullType)) // null is a 'object' type dataset
 
 
 const id1 = Symbol("12")
@@ -18,3 +20,18 @@ const id2 = Symbol("12")
 console.log(typeof(id1));
 
 console.log(id1 === id2);   // false even tho both symbol has same value input, creates a unique value for each variable
+
+
+//NON PRIMITIVE DATATYPES-----------------------------------
+const arrays = ["capibara","panda","cat"]
+console.log(arrays);
+
+let myobj = {name:"Gojo",age:22} // object data type
+console.log(myobj);
+
+// will get back to functions later on..heres a glance
+const myfunc = function(){
+    console.log("yay the function was run")
+}
+ myfunc()
+
