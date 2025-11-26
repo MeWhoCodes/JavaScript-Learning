587ab506faaed1dd67ae2b8eb147a69005e3365b
Devansh Sharma <devaton4@gmail.com>
2025-11-26 16:27:27 +0000

    notes on js added

---
+++ 01_basics/01_var_const.js
@@ -0,0 +1,18 @@
+const name = "Devansh"
+
+var email = "dev@gmail.com"
+
+let number = 1071180360
+let landline // landline var is defined but its 'undefined atm'
+
+aliveStatus = "Alive"
+
+// name = "Deva" // NOT ALLOWED, VARIABLE WAS DEFINED AS A CONSTANT, IMMUTABLE DATA TYPE
+
+/*
+            'let' is ought to be used for defining mutable variables, 'var' is now obselete
+            you CAN define variables without righting 'let', but YOU DARE NOT, THATS NOT A GOOD PRACTICE.
+*/
+
+
+console.table([name, email, number, landline, aliveStatus]);
