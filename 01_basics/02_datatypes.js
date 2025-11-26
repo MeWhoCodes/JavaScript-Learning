77e09d5fa0bbd5a4cbbeb1420e45fcc5963a35b7
Devansh Sharma <devaton4@gmail.com>
2025-11-26 16:59:03 +0000

    02 datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+let stringy = "hello"
+let num = 12
+let bool = false
+
+let undef = undefined
+let weirdType = null
+
+console.log(typeof undef);      // undefined is a 'undefined' type dataset
+console.log(typeof weirdType) // null is a 'object' type dataset
