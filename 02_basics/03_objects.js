f9bd24455d84e5af93f8703ce4a935953b203546
Devansh Sharma <devaton4@gmail.com>
2025-12-03 11:23:52 +0530

    created jsuser object

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,1 +1,11 @@
+// singleton is not created when defined by literal, only as constructors its a singleton
 
+//object literal
+const jsuser = {
+  name: "Marvis",
+  age: 13,
+  location: undefined,
+  email: "marv.12@hoomail.com",
+  isLoggedin: false,
+  lastLoggedIn: ["Tuesday","Sunday"]
+}
