f87c59d132854ef60673d92014ec432af76e99d1
Devansh Sharma <devaton4@gmail.com>
2025-12-17 14:27:07 +0000

    added function initializatoin methods

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,3 +1,42 @@
+//                                                method 1
+function addone(value){
+    return value+1
+}
+console.log(addone(3));
+//---------------------------------------------------------------
+
+//                                                method 2
+const addtwo = function (value){
+    return value+2
+}
+console.log(addtwo(3));
+//----------------------------------------------------------------
+//                                               Difference:
+
+
+//difference? functions of method 1 can have their call  before the function was created or after for example
+a()
+function a(){}
+
+//but not functions of method 2, they are like defined through variables, so need to be called only if its initialised
+
+//b() <------wont work
+const b = function (){}
+b() //<---works
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function sayMyName(name){
     console.log(`Hello ${name}, how have you been?`)
 }
