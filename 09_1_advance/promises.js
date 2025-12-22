b1ed44ddfca4b34b5e0adac2d30d6deacb4792e6
Devansh Sharma <devaton4@gmail.com>
2025-12-22 09:51:15 +0000

    promises

--- 09_1_advance/promises.js
+++ 09_1_advance/promises.js
@@ -1,5 +1,10 @@
 // promise => it WILL be execute, but not rn, but it will be..
 //has 3 states.
+
+
+//NOTE: WE CAN'T GET ANY VALUE OUT OF PROMISE AND STORE IT IN A VARIABLE
+
+
 const promiseOne = new Promise(function(resolve,reject){
     // DB async tasks
     //DB calls, crypto,network
@@ -34,7 +39,7 @@ new Promise(function(resolve, reject){
 
 const promiseTHREE = new Promise(function(resolve,reject){
     setTimeout(function(){
-        resolve({username: "Woody", email: "Woody@gmail.com "}); //we got this data from somewhere, now we will return it
+        resolve({username: "Woody", email: "Woody@googe.com "}); //we got this data from somewhere, now we will return it
     },3000)
 })
 
@@ -42,3 +47,44 @@ promiseTHREE.then(function(user){
     console.log(user);
     
 })
+
+
+
+// we can chain .then for piping async tasks
+
+const promiseFOUR = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let err = true                                                                  // toggle true or false to see differnt outcomes
+        if(!err){ // no error was found, so we found stuff from our query/database
+            resolve({username: "Bob", email : "bob@googe.com"})
+        }else{
+            reject("ERROR 324: Something Went wrong, couldn't get the query")
+        }
+    },4000)
+})
+
+
+promiseFOUR.then(function(info){
+    console.log("Query data was received")
+    console.log(info);
+    return [info.username, info.email]
+    
+}).then(function(dat){
+    console.log(dat);
+    console.log(`${dat[0]} and ${dat[1]}`);
+    
+    //now dat can be processed async
+     
+    
+}).catch(function(err){
+    console.log(err);
+    
+}). finally(function(){
+    console.log("runs when everything is done, either rejected or resolved, runs always after everything is set and done :))");
+    
+})
+
+
+
+
+    
